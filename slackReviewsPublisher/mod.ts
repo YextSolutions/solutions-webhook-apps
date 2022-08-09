0dc4cd85a5e55cfecb8e82740ba04ec724afe607
mattmalone212 <79661550+mattmalone212@users.noreply.github.com>
2022-08-09 12:57:59 -0400

    Update mod.ts

--- slackReviewsPublisher/mod.ts
+++ slackReviewsPublisher/mod.ts
@@ -1,11 +1,19 @@
-const TARGET_ENDPOINT = "${{slackWebhookUrl}}";
+const TARGET_ENDPOINT = "https://hooks.slack.com/services/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";
 
 export function handleNotification(data: any) {
 
     const type = data.meta.eventType; // this is from the webhook
-    if (type === "REVIEW_CREATED") {
+    const recommendation = data.review.recommendation
+    if (type === "REVIEW_CREATED" && typeof (recommendation) == 'undefined' ) {
         var publisherId = data.review.publisherId;
         var reviewRating = data.review.rating;
+        var reviewId = data.review.id;
+        var reviewUrl = "https://www.yext.com/s/3609847/reviews#p0=status&p1=1%7C3&p2=includes&p3=&yextReviewId=" + reviewId;
+        return updateMessageHandler(publisherId, reviewRating, reviewUrl);
+    }
+    else if (type === "REVIEW_CREATED" && typeof (recommendation) !== 'undefined' ) {
+        var publisherId = data.review.publisherId;
+        var reviewRating = data.review.recommendation;
         var reviewId = data.review.id;
         var reviewUrl = "https://www.yext.com/s/3609847/reviews#p0=status&p1=1%7C3&p2=includes&p3=&yextReviewId=" + reviewId;
         return updateMessageHandler(publisherId, reviewRating, reviewUrl);
@@ -14,7 +22,7 @@ export function handleNotification(data: any) {
 }
 
 export function updateMessageHandler(publisherId: string, reviewRating: number, reviewUrl: string) {
-    var message = "You received a new review! \nPublisher ID: " + publisherId + "\nReview Rating: " + reviewRating + "\nView the review here: " + reviewUrl
+    var message = "You received a new review! \nPublisher ID: " + publisherId + "\nReview Rating: " + reviewRating + "\nSee the URL here: " + reviewUrl
     return postRequest(message);
 }
 
