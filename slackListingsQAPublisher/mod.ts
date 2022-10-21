f0fafcc6f1d3e66dce1271ca9af86079fc39acfe
Lily <liwang@yext.com>
2022-10-21 11:34:23 -0400

    Update Listings QA mod.ts

--- slackListingsQAPublisher/mod.ts
+++ slackListingsQAPublisher/mod.ts
@@ -16,17 +16,29 @@ export function handleListingsQANotification(data: any) {
         var questionUrl = "yext.com/s/" + accountId + "/questions";
 
         return updateQuestionHandler(authorName, content, questionUrl);
+    } else if (type === "ANSWER_UPDATED") {
+        var accountId = data.meta.accountId;
+        var authorName = data.question.answers.map((obj) => obj.authorName)[0];
+        var content = data.question.answers.map((obj) => obj.content)[0];
+        var questionUrl = "yext.com/s/" + accountId + "/questions";
+
+        return updateAnswerHandler(authorName, content, questionUrl);
     }
     return null;
 }
 
 export function createQuestionHandler(authorName: string, content: string, questionUrl: string) {
-    var message = "A new question has been added to one of your listings by " + authorName + "!" + "\nQuestion: " + content + "\nView your Listings Q&A here: " + questionUrl;
+    var message = "A new question has been added to one of your listings by " + authorName + "!" + "\nNew Question: " + content + "\nView your Listings Q&A here: " + questionUrl;
     return postRequest(message);
 }
 
 export function updateQuestionHandler(authorName: string, content: string, questionUrl: string) {
-    var message = "A question has been updated on one of your listings!" + "\nAuthor: " + authorName + "\nQuestion: " + content + "\nView your Listings Q&A here: " + questionUrl;
+    var message = "A question has been updated on one of your listings!" + "\nAuthor: " + authorName + "\nUpdated Question: " + content + "\nView your Listings Q&A here: " + questionUrl;
+    return postRequest(message);
+}
+
+export function updateAnswerHandler(authorName: string, content: string, questionUrl: string) {
+    var message = "An answer has been updated on one of your listings!" + "\nAuthor: " + authorName + "\nUpdated Answer: " + content + "\nView your Listings Q&A here: " + questionUrl;
     return postRequest(message);
 }
 
